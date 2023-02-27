6d2bbe1a2386dbec09a34451699de923f7e9bfd6
Chuongnp <nguyenphucchuong97@gmail.com>
2023-02-27 20:53:42 +0700

    preload renderer

---
+++ renderer.js
@@ -0,0 +1,5 @@
+console.log(versions)
+
+
+const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
